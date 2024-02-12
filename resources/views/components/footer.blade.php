c24a1f9b1dd3b2395a8b64cc60997ad59492d3d1
aurorapuli <144793745+aurorapuli@users.noreply.github.com>
2024-02-12 17:38:52 +0100

    add:footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,3 +1,108 @@
 <footer>
-    <h1>Hello from footer</h1>
+    <section class="up">
+        <div class="container">
+            <div class="list">
+                <div>
+                    <img src="{{ Vite::asset('resources/img/buy-comics-digital-comics.png') }}" alt="">
+                </div>
+                <P>DIGITAL COMICS</P>
+            </div>
+            <div class="list">
+                <div>
+                    <img src="{{ Vite::asset('resources/img/buy-comics-merchandise.png') }}" alt="">
+                </div>
+                <P>DIGITAL COMICS</P>
+            </div>
+            <div class="list">
+                <div>
+                    <img src="{{ Vite::asset('resources/img/buy-comics-digital-comics.png') }}" alt="">
+                </div>
+                <P>DIGITAL COMICS</P>
+            </div>
+            <div class="list">
+                <div>
+                    <img src="{{ Vite::asset('resources/img/buy-comics-shop-locator.png') }}" alt="">
+                </div>
+                <P>DIGITAL COMICS</P>
+            </div>
+            <div class="list">
+                <div>
+                    <img src="{{ Vite::asset('resources/img/buy-comics-shop-locator.png') }}" alt="">
+                </div>
+                <P>DIGITAL COMICS</P>
+            </div>
+        </div>
+
+    </section>
+    <section class="down">
+        <div class="container">
+            <div>
+                <div>
+                    <ul>
+                        <h4>DC COMICS</h4>
+                        <li><a href="#">Comics</a></li>
+                        <li><a href="#">Movies</a></li>
+                        <li><a href="#">TV</a></li>
+                        <li><a href="#">Games</a></li>
+                        <li><a href="#">Videos</a></li>
+                        <li><a href="#">News</a></li>
+                    </ul>
+                    <ul>
+                        <h4>SHOP</h4>
+                        <li><a href="#">Shop DC</a></li>
+                        <li><a href="#">Shop DC Collection</a></li>
+
+                    </ul>
+
+                </div>
+                <div>
+                    <ul>
+                        <h4>DC</h4>
+                        <li><a href="#">Temes Of Use</a></li>
+                        <li><a href="#">Privacy policy(New)</a></li>
+                        <li><a href="#">Ad Choise</a></li>
+                        <li><a href="#">Advertising</a></li>
+                        <li><a href="#">jobs</a></li>
+                        <li><a href="#">Subscription</a></li>
+                        <li><a href="#">Talent Workshops</a></li>
+                        <li><a href="#">CPSC Certificates</a></li>
+                        <li><a href="#">Ratings</a></li>
+                        <li><a href="#">Shop Help</a></li>
+                        <li><a href="#">Contact Us</a></li>
+                    </ul>
+
+                </div>
+                <div>
+                    <ul>
+                        <h4>DC</h4>
+                        <li><a href="#">DC</a></li>
+                        <li><a href="#"> MAD Magazine</a></li>
+                        <li><a href="#">DC Kids</a></li>
+                        <li><a href="#">DC Universe</a></li>
+                        <li><a href="#">DC Power Visa</a></li>
+                    </ul>
+
+                </div>
+
+            </div>
+            <div class="img">
+                <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="">
+            </div>
+
+        </div>
+        <div class="sign-up">
+            <div class="container">
+                <a href="#">SIGN-UP NOW!</a>
+                <div class="social">
+                    <p><strong>FOLLOW US</strong></p>
+                    <img src="{{ Vite::asset('resources/img/footer-facebook.png') }}" alt="">
+                    <img src="{{ Vite::asset('resources/img/footer-periscope.png') }}" alt="">
+                    <img src="{{ Vite::asset('resources/img/footer-pinterest.png') }}" alt="">
+                    <img src="{{ Vite::asset('resources/img/footer-twitter.png') }}" alt="">
+                    <img src="{{ Vite::asset('resources/img/footer-youtube.png') }}" alt="">
+
+
+                </div>
+            </div>
+        </div>
 </footer>
