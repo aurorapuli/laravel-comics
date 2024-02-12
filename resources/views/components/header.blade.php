07d766679eaa4ab5f61346c0c17897b73c61170e
aurorapuli <144793745+aurorapuli@users.noreply.github.com>
2024-02-12 17:08:29 +0100

    add: img and header

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,3 +1,30 @@
 <header>
-    <h1>Hello from Header</h1>
+    <div class="container">
+        <div class="d-flex justify-content-between align-items-center">
+            <div>
+                <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="">
+            </div>
+            <div>
+                <ul class="d-flex justify-content-between">
+                    <li class="px-3">CHARACTERS</li>
+                    <li class="px-3">COMICS</li>
+                    <li class="px-3">MOVIES</li>
+                    <li class="px-3">TV</li>
+                    <li class="px-3">TV</li>
+                    <li class="px-3">GAMES</li>
+                    <li class="px-3">COLLECTIBLES</li>
+                    <li class="px-3">VIDEOS</li>
+                    <li class="px-3">FANS</li>
+                    <li class="px-3">FANS</li>
+                    <li class="px-3">NEWS</li>
+                    <li class="px-3">SHOP</li>
+                </ul>
+            </div>
+            <div><a href="#">Shop</a></div>
+        </div>
+    </div>
+    <div class="container-fluid jumbotron">
+
+
+    </div>
 </header>
