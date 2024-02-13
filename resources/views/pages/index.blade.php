3cde0a48da2d028e6b004458cab1924c5379a2fe
aurorapuli <144793745+aurorapuli@users.noreply.github.com>
2024-02-13 09:56:17 +0100

    add: cards

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -3,5 +3,23 @@
     <title>Home</title>
 @endsection
 @section('content')
-    <h1>Hello, World!</h1>
+    <main>
+
+        <div class="row-comics container py-5">
+
+            @foreach ($comics as $comic)
+                <div class="card-comics">
+                    <div class="img">
+                        <img src="{{ $comic['thumb'] }}">
+                    </div>
+
+                    <p>{{ $comic['title'] }}</p>
+
+                </div>
+            @endforeach
+
+        </div>
+
+
+    </main>
 @endsection
